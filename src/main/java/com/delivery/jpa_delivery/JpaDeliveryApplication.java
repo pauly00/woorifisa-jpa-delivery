0557f56ad16844f07e3c8a77c383a9316f1bce63
yujung23 <kimyj4017@gmail.com>
2026-03-10 14:36:53 +0900

    Initial Commit: 배달 시

---
+++ src/main/java/com/delivery/jpa_delivery/JpaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.jpa_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivery/jpa_delivery/JpaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.jpa_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
