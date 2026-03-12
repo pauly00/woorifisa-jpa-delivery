0146a10ea7a7e4c5259f74bf52758b98947d6f65
Minyeong <skaalsdud7@gmail.com>
2026-03-12 11:03:15 +0900

    feat: implement security config

---
+++ src/main/java/com/delivery/jpa_delivery/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.delivery.jpa_delivery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.util.Set;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/main", "/join", "/login").permitAll()
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .successHandler(customAuthenticationSuccessHandler())
+                        .permitAll())
+                .logout(logout -> logout.logoutSuccessUrl("/main"));
+
+        return http.build();
+    }
+
+    // 권한별 리다이렉트 로직을 담은 핸들러 빈 등록
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        return (request, response, authentication) -> {
+            Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+
+            if (roles.contains("ROLE_ADMIN")) {
+                response.sendRedirect("/admin/stores");
+            } else if (roles.contains("ROLE_USER")) {
+                response.sendRedirect("/user/orders");
+            } else {
+                response.sendRedirect("/main");
+            }
+        };
+    }
+}
